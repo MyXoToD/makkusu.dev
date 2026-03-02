e67dae4f4f9a5b87ced32ff79f49ba16efb40210
Maximilian Boll <myxotod@gmail.com>
2026-03-02 23:35:34 +0100

    Few content adjustments

--- eleventy.config.js
+++ eleventy.config.js
@@ -100,7 +100,11 @@ export default async function (eleventyConfig) {
   });
   eleventyConfig.addFilter('dateWithTime', (value) => {
     let date = new Date(value);
-    return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    return `${day}.${month}.${date.getFullYear()} ${hours}:${minutes}`;
   });
   eleventyConfig.addFilter('publishedPosts', (data) => {
     return data;
---
+++ src/pages/about/essentials.md
@@ -0,0 +1,11 @@
+---
+title: Essentials
+permalink: /essentials/
+---
+
+<div class="content content--narrow">
+<h1 class="page-title" data-kanji="毎日">Essentials</h1>
+
+Everyday items I carry whereever I go.
+
+</div>
